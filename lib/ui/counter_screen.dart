4fa569f8ca4d9b77a90826abc69df368fdec6664
akshay21022003 <akshayvilawadekar21@gmail.com>
2024-02-07 23:17:41 +0530

    counter application completed

--- lib/ui/counter_screen.dart
+++ lib/ui/counter_screen.dart
@@ -1,4 +1,8 @@
+import 'package:block_state_management/bloc/counter/counter_bloc.dart';
+import 'package:block_state_management/bloc/counter/counter_event.dart';
+import 'package:block_state_management/bloc/counter/counter_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
@@ -19,18 +23,26 @@ class _CounterScreenState extends State<CounterScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          const Text('0',
-          style: TextStyle(fontSize: 60),
-          ),
+          BlocBuilder<CounterBloc,CounterState>(
+            builder: (context,state){
+            return Text(state.counter.toString(),
+          style: const TextStyle(fontSize: 60),
+          );
+          }),
+          
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
-                onPressed: (){}, 
+                onPressed: (){
+                  context.read<CounterBloc>().add(IncrementCounter());
+                }, 
                 child: const Text('Increment')),
                 const SizedBox(width: 20,),
                 ElevatedButton(
-                onPressed: (){}, 
+                onPressed: (){
+                  context.read<CounterBloc>().add(DecrementCounter());
+                }, 
                 child: const Text('Decrement'))
             ],
           )
